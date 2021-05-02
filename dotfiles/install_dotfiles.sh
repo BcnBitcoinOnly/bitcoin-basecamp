29ec23fc6716cbaf304b5014e242b4846fee2b73
Federico <me@federicociro.com>
2021-05-02 19:18:31 +0100

    Completed dotfiles script

--- dotfiles/install_dotfiles.sh
+++ dotfiles/install_dotfiles.sh
@@ -1,4 +1,11 @@
-# /bin/sh
+# /bin/bash
 
-echo "This script will install the dotfiles in $USER and root"
+echo "Linking non-root files"
+ln -s /home/$USER/git/fresh-linux/dotfiles/server/not-root/.bash_aliases /home/$USER/.bash_aliases
+rm /home/$USER/.bashrc
+ln -s /home/$USER/git/fresh-linux/dotfiles/server/not-root/.bashrc /home/$USER/.bashrc
 
+echo "Linking root files"
+sudo ln -s /home/$USER/git/fresh-linux/dotfiles/server/root/.bash_aliases /root/.bash_aliases
+sudo rm /root/.bashrc
+sudo ln -s /home/$USER/git/dotfiles/server/root/.bashrc /root/.bashrc
