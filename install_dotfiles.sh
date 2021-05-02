a6e818d9ba47ea69415fa89c3fa9fbaf411e9b27
Federico <me@federicociro.com>
2021-05-02 19:51:01 +0100

    add .bash_profile link

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,8 +4,10 @@ echo "Linking non-root files"
 ln -s /home/$USER/git/fresh-linux/dotfiles/server/not-root/.bash_aliases /home/$USER/.bash_aliases
 rm /home/$USER/.bashrc
 ln -s /home/$USER/git/fresh-linux/dotfiles/server/not-root/.bashrc /home/$USER/.bashrc
+ln -s /home/$USER/git/fresh-linux/dotfiles/server/not-root/.bash_profile /home/$USER/.bash_profile
 
 echo "Linking root files"
 sudo ln -s /home/$USER/git/fresh-linux/dotfiles/server/root/.bash_aliases /root/.bash_aliases
 sudo rm /root/.bashrc
 sudo ln -s /home/$USER/git/fresh-linux/dotfiles/server/root/.bashrc /root/.bashrc
+sudo ln -s /home/$USER/git/fresh-linux/dotfiles/server/not-root/.bash_profile /root/.bash_profile
