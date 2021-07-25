bedd789af1dac262cec62cbf80916fded689a0a3
Federico <me@federicociro.com>
2021-07-25 11:17:26 +0200

    dotfiles rm

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -4,6 +4,7 @@ echo "Linking non-root files"
 ln -s /home/$USER/git/fresh-linux/dotfiles/server/not-root/.bash_aliases /home/$USER/.bash_aliases
 rm /home/$USER/.bashrc
 ln -s /home/$USER/git/fresh-linux/dotfiles/server/not-root/.bashrc /home/$USER/.bashrc
+rm /home/$USER/.bash_profile
 ln -s /home/$USER/git/fresh-linux/dotfiles/server/not-root/.bash_profile /home/$USER/.bash_profile
 ln -s /home/$USER/git/fresh-linux/dotfiles/server/not-root/.nanorc /home/$USER/.nanorc
 
@@ -11,4 +12,5 @@ echo "Linking root files"
 sudo ln -s /home/$USER/git/fresh-linux/dotfiles/server/root/.bash_aliases /root/.bash_aliases
 sudo rm /root/.bashrc
 sudo ln -s /home/$USER/git/fresh-linux/dotfiles/server/root/.bashrc /root/.bashrc
+sudo rm /root/.bash_profile
 sudo ln -s /home/$USER/git/fresh-linux/dotfiles/server/not-root/.bash_profile /root/.bash_profile
