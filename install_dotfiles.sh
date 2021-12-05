09bb57b63b8d912086954d59994f77e709b1604e
federico <git@federicociro.com>
2021-12-05 19:57:03 +0000

    new

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -6,7 +6,7 @@ if [[ $UID != 0 ]]; then
     exit 1
 fi
 
-echo "This script will install dotfiles in both non-root and root users"
+echo "This script will copy system configuration files and install dotfiles  in both non-root and root users"
 echo "Type the non-root user"
 read non_root
 
@@ -25,3 +25,15 @@ sudo rm -f /root/.bashrc
 sudo ln -s /home/$non_root/git/fresh-linux/dotfiles/server/root/.bashrc /root/.bashrc
 sudo rm -f /root/.bash_profile
 sudo ln -s /home/$non_root/.bash_profile /root/.bash_profile
+
+echo "Copying system configuration files"
+sudo ln -s /root/config/etc/asound.conf /etc/asound.conf
+sudo ln -s /root/config/etc/mpd.conf /etc/mpd.conf
+sudo mkdir /etc/spotifyd
+sudo ln -s /root/config/etc/spotifyd/* /etc/spotifyd
+sudo ln -s /root/config/etc/systemd/system/* /etc/systemd/system
+sudo ln -s /root/config/etc/logrotate.d/* /etc/logrotate.d
+sudo ln -s /root/config/var/spool/cron/crontabs/root /var/spool/cron/crontabs/
+sudo ln -s /root/config/etc/wireguard/wg0.conf /etc/wireguard
+mkdir /home/$non_root/.config/mpd
+sudo ln -s /root/config/home/user/.config/mpd/mpd.conf /home/feder/.config/mpd/
