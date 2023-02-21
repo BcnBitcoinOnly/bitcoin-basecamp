ac07cccf4e47631aa28b44e55ccaf6f142e5b61e
Federico B <25438748+federicociro@users.noreply.github.com>
2023-02-21 06:55:58 +0100

    Update bitcoin.sh

--- scripts/bitcoin.sh
+++ scripts/bitcoin.sh
@@ -6,58 +6,15 @@ cd bitcoin
 
 # Install dependencies
 sudo apt-get update
-sudo apt-get install \
-    automake \
-    autotools-dev \
-    bsdmainutils \
-    build-essential \
-    ccache \
-    clang \
-    gcc \
-    git \
-    libboost-dev \
-    libboost-filesystem-dev \
-    libboost-system-dev \
-    libboost-test-dev \
-    libevent-dev \
-    libminiupnpc-dev \
-    libnatpmp-dev \
-    libqt5gui5 \
-    libqt5core5a \
-    libqt5dbus5 \
-    libsqlite3-dev \
-    libtool \
-    libzmq3-dev \
-    pkg-config \
-    python3 \
-    qttools5-dev \
-    qttools5-dev-tools \
-    qtwayland5 \
-    systemtap-sdt-dev
+echo "Install Build requirements"
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config bsdmainutils python3
+echo "Install required dependencies"
+sudo apt-get install libevent-dev libboost-dev
 
 # Build the dependencies
 ./autogen.sh
-./contrib/install_db4.sh $(pwd)
 echo "You can run ./configure --help to see all the various configuration options"
-./configure \
-        BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" \
-        BDB_CFLAGS="-I${BDB_PREFIX}/include" \
-        --disable-bench \
-        --disable-gui-tests \
-        --disable-maintainer-mode \
-        #--disable-man \
-        --disable-tests \
-        --disable-zmq \
-        --enable-lto \
-        --with-daemon=yes \
-        --with-gui=no \
-        --with-libmultiprocess=no \
-        --with-libs=no \
-        --with-miniupnpc=no \
-        --with-mpgen=no \
-        --with-natpmp=no \
-        --with-qrencode=no \
-        --with-utils=yes
+./configure --disable-wallet
 
 # Compile
 make -j$(nproc)
