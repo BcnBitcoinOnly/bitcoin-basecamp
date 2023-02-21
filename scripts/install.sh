b1d82ecd462f44ac14dda3ba49a61d5f17c7a140
Federico B <25438748+federicociro@users.noreply.github.com>
2023-02-21 06:22:23 +0100

    fix the read prompt

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,8 +18,8 @@ if [ -n "$hostname" ]; then
 fi
 
 # Prompt the user to install ufw.
-    read -p "Install 'ufw' for a front-end for iptables? [Y/n] " -r
-    if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
+read -p "Install 'ufw' for a front-end for iptables? [Y/n] " REPLY
+if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
   sudo apt install ufw -y
   sudo ufw limit ssh
   sudo ufw enable
@@ -30,85 +30,85 @@ fi
 
 # Prompt to install utilities
     # Fail2ban: an intrusion prevention software framework
-    read -p "Install 'fail2ban' for intrusion prevention? [Y/n] " -r
+    read -p "Install 'fail2ban' for intrusion prevention? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install fail2ban
     fi
 
     # tldr: Simplified and community-driven man pages
-    read -p "Install 'tldr' for simplified man pages? [Y/n] " -r
+    read -p "Install 'tldr' for simplified man pages? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install tldr
     fi
 
     # tree: List contents of directories in a tree-like format
-    read -p "Install 'tree' for directory listing? [Y/n] " -r
+    read -p "Install 'tree' for directory listing? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install tree
     fi
 
     # logrotate: Rotate, compress, and remove log files
-    read -p "Install 'logrotate' for log rotation? [Y/n] " -r
+    read -p "Install 'logrotate' for log rotation? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install logrotate
     fi
 
     # lnav: Log file navigator
-    read -p "Install 'lnav' for log file navigation? [Y/n] " -r
+    read -p "Install 'lnav' for log file navigation? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install lnav
     fi
 
     # dnsutils: DNS utilities, including dig and nslookup
-    read -p "Install 'dnsutils' for DNS utilities (dig, nslookup, etc.)? [Y/n] " -r
+    read -p "Install 'dnsutils' for DNS utilities (dig, nslookup, etc.)? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install dnsutils
     fi
 
     # qrencode: Encode and decode QR codes
-    read -p "Install 'qrencode' for QR code encoding and decoding? [Y/n] " -r
+    read -p "Install 'qrencode' for QR code encoding and decoding? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install qrencode
     fi
 
     # borgbackup: Deduplicated and encrypted backups
-    read -p "Install 'borgbackup' for encrypted backups? [Y/n] " -r
+    read -p "Install 'borgbackup' for encrypted backups? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install borgbackup
     fi
 
     # rsync: Efficient file synchronization
-    read -p "Install 'rsync' for efficient file synchronization? [Y/n] " -r
+    read -p "Install 'rsync' for efficient file synchronization? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install rsync
     fi
 
     # rclone: Sync files and directories to/from cloud services
-    read -p "Install 'rclone' for syncing with cloud services? [Y/n] " -r
+    read -p "Install 'rclone' for syncing with cloud services? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install rclone
     fi
 
     # net-tools: Network configuration utilities (ifconfig, netstat, etc.)
-    read -p "Install 'net-tools' for network configuration utilities? [Y/n] " -r
+    read -p "Install 'net-tools' for network configuration utilities? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install net-tools
     fi
 
     # locate: A command-line utility that helps you find files and directories on your system
-    read -p "Install 'locate' for efficient file synchronization? [Y/n] " -r
+    read -p "Install 'locate' for efficient file synchronization? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install locate
     fi
 
     # ncdu: A disk usage analyzer with an ncurses interface
-    read -p "Install 'ncdu' for efficient file synchronization? [Y/n] " -r
+    read -p "Install 'ncdu' for efficient file synchronization? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install ncdu
     fi
 
     # curl: A command-line tool for transferring data from or to a server, using one of the supported protocols (HTTP, FTP, etc.)
-    read -p "Install 'curl' for efficient file synchronization? [Y/n] " -r
+    read -p "Install 'curl' for efficient file synchronization? [Y/n] " REPLY
     if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
         sudo apt install curl
     fi
