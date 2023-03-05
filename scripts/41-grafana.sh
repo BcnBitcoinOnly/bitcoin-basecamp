97d66000cb379cbb463066f3dde3b95815aceb44
Federico B <25438748+federicociro@users.noreply.github.com>
2023-03-05 21:41:22 +0100

    Update 41-grafana.sh

--- scripts/41-grafana.sh
+++ scripts/41-grafana.sh
@@ -15,12 +15,15 @@ echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com st
 sudo apt-get update
 sudo apt-get install -y grafana
 
+# Reverse proxy
+sudo mkdir -p /etc/grafana/provisioning/datasources
+sudo cp $script_loc/../etc/grafana/provisioning/datasources/prometheus.yaml /etc/grafana/provisioning/datasources/
+sudo cp $script_loc/../config/etc/nginx/sites-available/grafana.conf /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/grafana.conf /etc/nginx/sites-enabled/
+
 # Start the server with systemd
 sudo systemctl daemon-reload
 sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
 sudo systemctl status grafana-server
 
-# Reverse proxy
-sudo cp $script_loc/../config/etc/nginx/sites-available/grafana.conf /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/grafana.conf /etc/nginx/sites-enabled/
