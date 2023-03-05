67b33338371cc8cd5a470bcc2a27b52d2a257e08
Federico B <25438748+federicociro@users.noreply.github.com>
2023-03-05 19:15:40 +0100

    Create 41-grafana.sh

---
+++ scripts/41-grafana.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+script_loc=$(pwd)
+
+# Install and configure Grafana
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y software-properties-common
+sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get install -y grafana
+
+# Start the server with systemd
+sudo systemctl daemon-reload
+sudo systemctl enable grafana-server
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
